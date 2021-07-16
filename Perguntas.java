517783132be0bf49a768925104254259ad95e4a3
Alexandre Palles <61762191+Alexandre-Palles@users.noreply.github.com>
2021-07-15 23:54:39 -0300

    nome das variaveis modificado

--- Perguntas.java
+++ Perguntas.java
@@ -46,60 +46,60 @@ public class Perguntas extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Qual a porcentagem de emissões de dióxido");
-		lblNewLabel.setForeground(Color.WHITE);
-		lblNewLabel.setHorizontalAlignment(SwingConstants.EAST);
-		lblNewLabel.setFont(new Font("Pagul", Font.BOLD, 16));
-		lblNewLabel.setBounds(233, 209, 337, 31);
-		contentPane.add(lblNewLabel);
+		JLabel lblTrecho_inicial = new JLabel("Qual a porcentagem de emissões de dióxido");
+		lblTrecho_inicial.setForeground(Color.WHITE);
+		lblTrecho_inicial.setHorizontalAlignment(SwingConstants.EAST);
+		lblTrecho_inicial.setFont(new Font("Pagul", Font.BOLD, 16));
+		lblTrecho_inicial.setBounds(233, 209, 337, 31);
+		contentPane.add(lblTrecho_inicial);
 		
-		JLabel lblNewLabel_1 = new JLabel("de carbono que são absorvidas pelos");
-		lblNewLabel_1.setForeground(new Color(255, 255, 255));
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setFont(new Font("Dialog", Font.BOLD, 16));
-		lblNewLabel_1.setBounds(247, 239, 312, 15);
-		contentPane.add(lblNewLabel_1);
+		JLabel lblTrecho_meio = new JLabel("de carbono que são absorvidas pelos");
+		lblTrecho_meio.setForeground(new Color(255, 255, 255));
+		lblTrecho_meio.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTrecho_meio.setFont(new Font("Dialog", Font.BOLD, 16));
+		lblTrecho_meio.setBounds(247, 239, 312, 15);
+		contentPane.add(lblTrecho_meio);
 		
-		JLabel lblNewLabel_1_1 = new JLabel("Geografia");
-		lblNewLabel_1_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1_1.setForeground(Color.WHITE);
-		lblNewLabel_1_1.setFont(new Font("Pagul", Font.BOLD, 57));
-		lblNewLabel_1_1.setBounds(247, 127, 312, 84);
-		contentPane.add(lblNewLabel_1_1);
+		JLabel lblCategoria = new JLabel("Geografia");
+		lblCategoria.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCategoria.setForeground(Color.WHITE);
+		lblCategoria.setFont(new Font("Pagul", Font.BOLD, 57));
+		lblCategoria.setBounds(247, 127, 312, 84);
+		contentPane.add(lblCategoria);
 		
-		JLabel lblNewLabel_3 = new JLabel("oceanos todos os anos?");
-		lblNewLabel_3.setForeground(Color.WHITE);
-		lblNewLabel_3.setFont(new Font("Pagul", Font.BOLD, 16));
-		lblNewLabel_3.setBounds(307, 261, 200, 15);
-		contentPane.add(lblNewLabel_3);
+		JLabel lblTrecho_final = new JLabel("oceanos todos os anos?");
+		lblTrecho_final.setForeground(Color.WHITE);
+		lblTrecho_final.setFont(new Font("Pagul", Font.BOLD, 16));
+		lblTrecho_final.setBounds(307, 261, 200, 15);
+		contentPane.add(lblTrecho_final);
 		
-		JButton btnNewButton = new JButton("A. 10% - 20%");
-		btnNewButton.setFont(new Font("Pagul", Font.BOLD, 20));
-		btnNewButton.setBackground(new Color(211, 214, 216));
-		btnNewButton.setForeground(new Color(5, 61, 87));
-		btnNewButton.setBounds(155, 309, 169, 52);
-		contentPane.add(btnNewButton);
+		JButton btnLetraA = new JButton("A. 10% - 20%");
+		btnLetraA.setFont(new Font("Pagul", Font.BOLD, 20));
+		btnLetraA.setBackground(new Color(211, 214, 216));
+		btnLetraA.setForeground(new Color(5, 61, 87));
+		btnLetraA.setBounds(155, 309, 169, 52);
+		contentPane.add(btnLetraA);
 		
-		JButton btnNewButton_1 = new JButton("B. 20% - 30%");
-		btnNewButton_1.setFont(new Font("Pagul", Font.BOLD, 20));
-		btnNewButton_1.setBackground(new Color(211, 214, 216));
-		btnNewButton_1.setForeground(new Color(5, 61, 87));
-		btnNewButton_1.setBounds(485, 309, 169, 52);
-		contentPane.add(btnNewButton_1);
+		JButton btnLetraB = new JButton("B. 20% - 30%");
+		btnLetraB.setFont(new Font("Pagul", Font.BOLD, 20));
+		btnLetraB.setBackground(new Color(211, 214, 216));
+		btnLetraB.setForeground(new Color(5, 61, 87));
+		btnLetraB.setBounds(485, 309, 169, 52);
+		contentPane.add(btnLetraB);
 		
-		JButton btnNewButton_2 = new JButton("C. 40% - 50%");
-		btnNewButton_2.setFont(new Font("Pagul", Font.BOLD, 20));
-		btnNewButton_2.setBackground(new Color(211, 214, 216));
-		btnNewButton_2.setForeground(new Color(5, 61, 87));
-		btnNewButton_2.setBounds(155, 442, 169, 52);
-		contentPane.add(btnNewButton_2);
+		JButton btnLetraC = new JButton("C. 40% - 50%");
+		btnLetraC.setFont(new Font("Pagul", Font.BOLD, 20));
+		btnLetraC.setBackground(new Color(211, 214, 216));
+		btnLetraC.setForeground(new Color(5, 61, 87));
+		btnLetraC.setBounds(155, 442, 169, 52);
+		contentPane.add(btnLetraC);
 		
-		JButton btnNewButton_3 = new JButton("D. 50%");
-		btnNewButton_3.setFont(new Font("Pagul", Font.BOLD, 20));
-		btnNewButton_3.setBackground(new Color(211, 214, 216));
-		btnNewButton_3.setForeground(new Color(5, 61, 87));
-		btnNewButton_3.setBounds(485, 442, 169, 52);
-		contentPane.add(btnNewButton_3);
+		JButton btnLetraD = new JButton("D. 50%");
+		btnLetraD.setFont(new Font("Pagul", Font.BOLD, 20));
+		btnLetraD.setBackground(new Color(211, 214, 216));
+		btnLetraD.setForeground(new Color(5, 61, 87));
+		btnLetraD.setBounds(485, 442, 169, 52);
+		contentPane.add(btnLetraD);
 		
 		JLabel lblNewLabel_2 = new JLabel("Background");
 		lblNewLabel_2.setForeground(Color.BLACK);
